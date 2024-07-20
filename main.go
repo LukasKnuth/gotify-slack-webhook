ae19b968117a6cbce7f863fb66ffc8f9920d79cb
Lukas Knuth <me@lknuth.dev>
2024-07-21 00:19:57 +0200

    Hacked together a working test.
    We use the Application Token in the Webhook URL like Slack does it. Since the Slack Webhook URLs contian secrets (and are therefor considered secrest), I expect applications to take precautions around this value already.
    
    There is no simple API to send messages on behalf of another App. So we simply sent the notifiaction against the local REST API with Markdown formating. There are configuration options where this will fail (namely when port/endpoint are specified outside of ENV variables). These should be in the readme AND the message.

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gotify/plugin-api"
@@ -41,16 +45,52 @@ func (c *Plugin) SetMessageHandler(msgHandler plugin.MessageHandler) {
 func (c *Plugin) RegisterWebhook(basePath string, mux *gin.RouterGroup) {
 	// TODO Does it make sense to use ENV to override the base path? Allows cluster-internal trafik only
 	c.basePath = basePath
-	mux.POST(webhook_path, func(req *gin.Context) {
+	mux.POST(webhook_path, func(endpoint *gin.Context) {
 		msg, err := ToMessage()
 		if err != nil {
-			req.String(http.StatusBadRequest, "Could not parse body to BlockKit")
+			endpoint.String(http.StatusBadRequest, "Could not parse body to BlockKit")
 		} else {
-			//token := req.Param("app_token")
-			c.msgHandler.SendMessage(msg)
-			req.String(http.StatusOK, "OK")
+			err = sendMessage(&msg, endpoint.Param("app_token"))
+			if err != nil {
+				fmt.Print(err.Error())
+				c.msgHandler.SendMessage(plugin.Message{
+					Title:   "Hook delivery failed!",
+					Message: err.Error(),
+				})
+			}
+			endpoint.String(http.StatusOK, "OK")
 		}
 	})
+}
+
+func sendMessage(msg *plugin.Message, token string) error {
+	port, is_set := os.LookupEnv("GOTIFY_SERVER_PORT")
+	if !is_set {
+		// TODO if the port is set via config, we're out of luck... Document in Readme?
+		// TODO same for listening address. Can we even do anything then?
+		port = "80"
+	}
+	url := fmt.Sprintf("http://localhost:%v/message", port)
+	json, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(json))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("X-Gotify-Key", token)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return errors.New("Gotify API indicated message sending failed. Is the token correct?")
+	} else {
+		return nil
+	}
 }
 
 func (c *Plugin) GetDisplay(location *url.URL) string {
