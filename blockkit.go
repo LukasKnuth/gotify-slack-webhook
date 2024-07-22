0027984aed27466967c81caad18fc8f679614f24
Lukas Knuth <me@lknuth.dev>
2024-07-22 23:11:48 +0200

    Webhook Parsing: Prototype implementation
    This uses gjson instead of the default JSON parser which requires structs, because it's much more applicable to how Slack BlockKit JSON structure works. We need to often chose specific implementations based on property values, which is easier with more control over the parsing pipeline.

--- blockkit.go
+++ blockkit.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io"
+
 	"github.com/gotify/plugin-api"
+	"github.com/tidwall/gjson"
 )
 
 func ToMessage() (plugin.Message, error) {
@@ -10,4 +14,81 @@ func ToMessage() (plugin.Message, error) {
 		Message: "Recieved this from Slack...\n\nSome **Markdown** going _down_ right here!",
 		Extras:  map[string]interface{}{"client::display": map[string]string{"contentType": "text/markdown"}},
 	}, nil
+}
+
+type Skip bool
+type Block interface {
+	Parse(block *gjson.Result) (Skip, error)
+	Render(out io.Writer) error
+}
+
+type WebhookBody struct {
+	Text   string // TODO both in the same document might not be valid...
+	Blocks []Block
+}
+
+func Parse(json string) (*WebhookBody, error) {
+	body := &WebhookBody{}
+	result := gjson.Get(json, "text")
+	if result.Exists() {
+		body.Text = result.String()
+	}
+	result = gjson.Get(json, "blocks")
+	for _, block := range result.Array() {
+		parsed, skip, err := parseBlock(&block)
+		if err != nil {
+			return nil, err
+		} else if parsed == nil || skip {
+			continue
+		} else {
+			body.Blocks = append(body.Blocks, parsed)
+		}
+	}
+	return body, nil
+}
+
+func parseBlock(block *gjson.Result) (Block, Skip, error) {
+	switch block.Get("type").String() {
+	case "section":
+		section := &SectionBlock{}
+		skip, err := section.Parse(block)
+		return section, skip, err
+	default:
+		return nil, true, nil
+	}
+}
+
+// -------- NEW FILE LAter -----------
+
+type SectionBlock struct {
+	Text   string
+	Fields []string
+	// TODO Accessory (either image or button - with URL)
+}
+
+func (sb *SectionBlock) Parse(block *gjson.Result) (Skip, error) {
+	if text := block.Get("text.text"); text.Exists() {
+		sb.Text = text.String()
+	}
+	block.Get("fields.#.text").ForEach(func(_, value gjson.Result) bool {
+		sb.Fields = append(sb.Fields, value.String())
+		return true
+	})
+	return false, nil
+}
+
+func (sb *SectionBlock) Render(out io.Writer) error {
+	if len(sb.Text) > 1 {
+		_, err := fmt.Fprintln(out, sb.Text)
+		if err != nil {
+			return err
+		}
+	}
+	for _, text := range sb.Fields {
+		_, err := fmt.Fprintln(out, text)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
---
+++ blockkit_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const exampleBody = `{
+    "text": "Danny Torrence left a 1 star review for your property.",
+    "blocks": [
+    	{
+    		"type": "section",
+    		"text": {
+    			"type": "mrkdwn",
+    			"text": "Danny Torrence left the following review for your property:"
+    		}
+    	},
+    	{
+    		"type": "section",
+    		"block_id": "section567",
+    		"text": {
+    			"type": "mrkdwn",
+    			"text": "<https://example.com|Overlook Hotel> \n :star: \n Doors had too many axe holes, guest in room 237 was far too rowdy, whole place felt stuck in the 1920s."
+    		},
+    		"accessory": {
+    			"type": "image",
+    			"image_url": "https://is5-ssl.mzstatic.com/image/thumb/Purple3/v4/d3/72/5c/d3725c8f-c642-5d69-1904-aa36e4297885/source/256x256bb.jpg",
+    			"alt_text": "Haunted hotel image"
+    		}
+    	},
+    	{
+    		"type": "section",
+    		"block_id": "section789",
+    		"text": {
+    			"type": "plain_text"
+    			"text": "Rating"
+    		}
+    		"fields": [
+    			{
+    				"type": "mrkdwn",
+    				"text": "*1.0*"
+    			},
+    			{
+    				"type": "plain_text",
+    				"text": "Not a good time"
+    			}
+    		]
+    	}
+    ]
+}
+`
+
+func TestParse(t *testing.T) {
+	body, err := Parse(exampleBody)
+	assert.Nil(t, err)
+	assert.Equal(t, "Danny Torrence left a 1 star review for your property.", body.Text)
+	assert.Len(t, body.Blocks, 3)
+
+	buf := new(bytes.Buffer)
+	err = body.Blocks[0].Render(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, "Danny Torrence left the following review for your property:\n", buf.String())
+
+	buf = new(bytes.Buffer)
+	err = body.Blocks[2].Render(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, "Rating\n*1.0*\nNot a good time\n", buf.String())
+}
