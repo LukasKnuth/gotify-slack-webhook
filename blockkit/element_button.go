43e1790a88358b540b053662d3b48991cb7679e3
Lukas Knuth <me@lknuth.dev>
2024-07-23 22:24:24 +0200

    Implemented ButtonElement, the first (and only) interactive block.
    Most interactive blocks rely on Slack custom support and API for interactions. However, a Button with a URL, that we can render. Bonus points for a scary emoji if the action requires confirmation and might be destructive.

---
+++ blockkit/element_button.go
@@ -0,0 +1,54 @@
+package blockkit
+
+import (
+	"github.com/lukasknuth/gotify-slack-webhook/gotify"
+	"github.com/tidwall/gjson"
+)
+
+type ButtonElement struct {
+	Text    *TextObject
+	Url     string
+	Confirm bool // TODO add a Emoji/Note?
+}
+
+func (be *ButtonElement) Parse(json *gjson.Result) (Skip, error) {
+	if url := json.Get("url"); !url.Exists() {
+		// Buttons without URLs are allowed, but we don't support them.
+		return true, nil
+	} else {
+		be.Url = url.String()
+		// The actual confirmation logic is irrelevant, just that the sender whats the action confirmed first
+		be.Confirm = json.Get("confirm").Exists()
+		text := json.Get("text")
+		text_object, skip, err := ToTextObject(&text)
+		if !skip && err == nil {
+			be.Text = text_object
+			return false, nil
+		} else {
+			return skip, err
+		}
+	}
+}
+
+func (be *ButtonElement) Render(out *gotify.MarkdownWriter) error {
+	if be.Confirm {
+		if err := out.WriteMarkdown("⚠️"); err != nil {
+			return err
+		}
+	}
+	if err := out.WriteMarkdown("["); err != nil {
+		return err
+	}
+	if err := be.Text.Render(out); err != nil {
+		return err
+	}
+	if err := out.WriteMarkdownF("](%s)", be.Url); err != nil {
+		return err
+	}
+	if be.Confirm {
+		if err := out.WriteMarkdown("⚠️"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
