30d4dddd6a52020e48c825c982ed9b72ea5ade90
Lukas Knuth <me@lknuth.dev>
2024-07-23 22:20:06 +0200

    MarkdownWriter supports format/newline now.

--- gotify/markdown_writer.go
+++ gotify/markdown_writer.go
@@ -18,17 +18,45 @@ func Wrap(wrap io.Writer) *MarkdownWriter {
 	}
 }
 
+func (mdw *MarkdownWriter) NewLine() error {
+	_, err := fmt.Fprint(mdw.Writer, "\n")
+	return err
+}
+
 func (mdw *MarkdownWriter) WriteMarkdown(data string) error {
+	_, err := fmt.Fprint(mdw.Writer, data)
+	return err
+}
+
+func (mdw *MarkdownWriter) WriteMarkdownLn(data string) error {
 	_, err := fmt.Fprintln(mdw.Writer, data)
 	return err
 }
 
+func (mdw *MarkdownWriter) WriteMarkdownF(format string, data ...any) error {
+	_, err := fmt.Fprintf(mdw.Writer, format, data...)
+	return err
+}
+
 func (mdw *MarkdownWriter) WritePlainText(data string) error {
-	_, err := mdw.escaper.WriteString(mdw.Writer, data)
+	return mdw.writeEscape(data)
+}
+
+func (mdw *MarkdownWriter) WritePlainTextLn(data string) error {
+	err := mdw.writeEscape(data)
 	if err != nil {
 		return err
 	} else {
-		_, err = fmt.Fprint(mdw.Writer, "\n")
-		return err
+		return mdw.NewLine()
 	}
+}
+
+func (mdw *MarkdownWriter) WritePlainTextF(format string, data ...any) error {
+	formatted := fmt.Sprintf(format, data...)
+	return mdw.writeEscape(formatted)
+}
+
+func (mdw *MarkdownWriter) writeEscape(text string) error {
+	_, err := mdw.escaper.WriteString(mdw.Writer, text)
+	return err
 }
